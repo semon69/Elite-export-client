b9fcd5fb3bade4b3db48fe73eb52b8a769ec88a3
semon69 <smdemon3@gmail.com>
2023-06-07 13:51:09 +0600

    add footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Components/Shared/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
