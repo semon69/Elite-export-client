28723e09e4a081696ec58b526b077892468fa400
semon69 <smdemon3@gmail.com>
2023-06-10 00:56:03 +0600

    add my enrolled class

--- src/Components/Dashboard/MyEnrollClass/MyEnrollClass.jsx
+++ src/Components/Dashboard/MyEnrollClass/MyEnrollClass.jsx
@@ -1,9 +1,63 @@
-import React from 'react';
+
+import React, { useContext } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { AuthContext } from '../../Provider/AuthProvider';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 
 const MyEnrollClass = () => {
+    const { user } = useContext(AuthContext)
+    const [axiosSecure] = useAxiosSecure()
+    const { data: myEnrollClass = [] } = useQuery({
+        queryKey: ['myEnrollClass', user?.email],
+        queryFn: async () => {
+            const res = await axiosSecure(`/myEnrollClass?email=${user?.email}`)
+            return res.data
+        }
+    })
+    console.log(myEnrollClass);
     return (
         <div>
-            <h1>My Enrolled class</h1>
+            <h1>My Enrolled class: {myEnrollClass.length}</h1>
+            <div className="overflow-x-auto border m-5">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>
+                                <p>Image</p>
+                            </th>
+                            <th>Name</th>
+                            <th>Instructor Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myEnrollClass.map((singleClass, index) =>
+                                <tr key={singleClass._id}>
+                                    <th>
+                                        {index + 1}
+                                    </th>
+                                    <td>
+                                        <div className="flex items-center space-x-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={singleClass.image} alt="Image" />
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </td>
+                                    <td>{singleClass.name}</td>
+                                    <td>{singleClass.instructorName}</td>
+                                </tr>
+                            )
+                        }
+
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
